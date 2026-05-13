63b70c0f7876676ce63276e258ff791c6343de13
Bulbul-Programing <bulbul.programing@gmail.com>
2026-05-13 16:24:10 +0600

    Add query builder

---
+++ src/app/builder/QueryBuilder.ts
@@ -0,0 +1,84 @@
+type QueryParams = Record<string, unknown>
+
+class QueryBuilder<T> {
+    public query: QueryParams;
+    public prismaQuery: any;
+
+    constructor(query: QueryParams) {
+        this.query = query
+    }
+
+    searching(searchableFields: string[]) {
+        const searchTerm = this.query.searchTerm as string
+
+        if (!searchTerm) return this
+
+        const orConditions = searchableFields.map((field) => {
+            if (field.includes('.')) {
+                const [relation, relationField] = field.split('.')
+
+                return {
+                    [relation]: {
+                        [relationField]: { contains: searchTerm, mode: "insensitive" }
+                    }
+                }
+            }
+
+            return {
+                [field]: {
+                    contains: searchTerm,
+                    mode: 'insensitive',
+                },
+            };
+
+        })
+
+        this.prismaQuery.where = {
+            ...this.prismaQuery.where,
+            OR: orConditions,
+        }
+        return this;
+    }
+
+
+
+    sort() {
+        const sort = this.query.sort as string;
+
+        if (sort) {
+            this.prismaQuery.orderBy = sort.split(',').map((field) => {
+                if (field.startsWith('-')) {
+                    return { [field.slice(1)]: 'desc' };
+                }
+                return { [field]: 'asc' };
+            });
+        } else {
+            this.prismaQuery.orderBy = { createdAt: 'desc' };
+        }
+
+        return this;
+    }
+
+    paginate() {
+        const page = Number(this.query.page) || 1;
+        const limit = Number(this.query.limit) || 10;
+
+        this.prismaQuery.skip = (page - 1) * limit;
+        this.prismaQuery.take = limit;
+
+        return this;
+    }
+
+    fields() {
+        if (this.query.fields) {
+            this.prismaQuery.select = (this.query.fields as string)
+                .split(',')
+                .reduce((acc, field) => {
+                    acc[field] = true;
+                    return acc;
+                }, {} as Record<string, boolean>);
+        }
+
+        return this;
+    }
+}
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../DBConfig/db";
 import AppError from "../../middleware/AppError";
-import { TUser } from "./user.interface";
+import { TUser, UserRole } from "./user.interface";
 import { envVars } from "../../envConfig";
 
 
@@ -23,6 +23,29 @@ const createUser = async (payload: TUser) => {
 
     return createUser
 }
+
+const crateAdmin = async (payload: TUser) => {
+    const isExistUser = await prisma.user.findUnique({
+        where: { email: payload.email }
+    })
+
+    if (isExistUser) {
+        throw new AppError(400, 'User already exist!')
+    }
+
+    const hashPassword = await bcrypt.hash(payload.password, Number(envVars.BCRYPT_ROUNDS))
+    payload.password = hashPassword
+    payload.role = UserRole.ADMIN
+
+    const createUser = await prisma.user.create(
+        {
+            data: payload
+        }
+    )
+
+    return createUser
+}
+
 
 export const userService = {
     createUser
